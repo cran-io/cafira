c14f039d07fc3fd4abb9e7a813a5b66fe1049d61
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2015-10-30 13:04:42 -0300

    Initializing everything for testing with RSpec

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Cafira
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Buenos Aires'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -31,5 +31,9 @@ module Cafira
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |g|
+      g.test_framework  :rspec, :fixture => true, :views => false
+      g.integration_tool :rspec, :fixture => true, :views => false
+    end
   end
 end
