6f7f47ea606e7b03d88ff661b2221d6b29e0d457
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2015-11-17 14:56:51 -0300

    Expositions index

--- app/admin/exposition.rb
+++ app/admin/exposition.rb
@@ -1,17 +1,38 @@
-ActiveAdmin.register Exposition do
+ActiveAdmin.register Exposition, :as => "Exposiciones" do
+  menu label: "Exposiciones"
   permit_params :ends_at, :initialized_at, :name, :active
+  config.batch_actions = false
+  index do
+    h2 "Lista de exposiciones"
+    br
+    column "Nombre", :name
+    column "Comenzada el", :initialized_at
+    column "Termina el", :ends_at
+    column "Activo", :active, :class => 'text-right'
+    column "Acciones" do |exposition|
+      link_to 'Activar', activate_home_exposicione_path(exposition) , :method => :post 
+    end
+    actions
+  end
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
 
+  action_item :activate do
+  end
+
+  filter :name, :label => "Nombre exposición"
+  filter :initialized_at, :label => "Comenzada el"
+  filter :ends_at, :label => "Termina el"
+  filter :active, :label => "Activo"
+
+  member_action :activate, method: :post do
+    if resource.active?
+      resource.active = false
+      resource.save
+    else
+      resource.active = true
+      resource.save
+    end
+    redirect_to home_exposiciones_path, notice: "Exposición #{resource.name} safisfactoriamente."
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
-  root 'home/expositions#index'
+  root 'home/exposiciones#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
