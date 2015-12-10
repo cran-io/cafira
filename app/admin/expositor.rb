e5802fabe6bcad5d97842d9b9bd35195a1b73d84
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2015-12-10 00:28:51 -0300

    Added sidebar to expositors

--- app/admin/expositor.rb
+++ app/admin/expositor.rb
@@ -1,46 +1,77 @@
-ActiveAdmin.register Expositor, :as => "Expositores" do
+ActiveAdmin.register Expositor do
   menu false
+  permit_params :name, :email, :cuit
+
   controller do
     def scoped_collection
       if params[:exposition_id]
-        @expositors ||= Exposition.find(params[:exposition_id]).expositors
+        @expositors = Exposition.find(params[:exposition_id]).expositors
       else
         @expositors = Expositor.all
       end
     end 
   end
 
-  index do
-    if params[:exposition_id]
-      h2 "Expositores en \"" + Exposition.find(params[:exposition_id]).name + "\"" 
-    else
-      h2 "Expositores"
-    end
-    column "Nombre", :name
-    column "Cuit", :cuit
-    column "E-mail", :email
-    column "Catálogo" do |expositor|
-      span do
-        link_to 'Editar', {:controller => 'home/catalogs', :action => 'index', :expositor => expositor.id}, :method => :get
+  sidebar "Acciones del expositor", :priority => 0, :except => :index do
+    ul do
+      li do
+        span do
+          link_to 'Credenciales',  home_expositor_credentials_path(resource), :method => :get
+        end
       end
-    end
-    column "Credenciales" do |expositor|
-      span do
-        link_to 'Editar', {:controller => 'home/credentials', :action => 'index', :expositor => expositor.id}, :method => :get
+      li do
+        span do
+          link_to 'Servicios Adicionales',  '', :method => :get
+        end
       end
-    end
-    column "Infrastructura" do |expositor|
-      span do
-        link_to 'Editar', {:controller => 'home/infrastructures', :action => 'index', :expositor => expositor.id}, :method => :get
+      li do
+        span do
+          link_to 'Catálogo',  '', :method => :get
+        end
       end
-    end
-    column "Servicios adicionales" do |expositor|
-      span do
-        link_to 'Editar', {:controller => 'home/aditional_services', :action => 'index', :expositor => expositor.id}, :method => :get
+      li do
+        span do
+          link_to 'Infraestructura',  '', :method => :get
+        end
       end
     end
   end
 
+  index do
+    if params[:exposition_id]
+      h2 "Expositors en \"" + Exposition.find(params[:exposition_id]).name + "\"" 
+    else
+      h2 "Expositors"
+    end
+    column "Nombre", :name
+    column "Cuit", :cuit
+    column "E-mail", :email
+    actions
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs 'Expositor' do
+      f.input :name, :label => "Nombre"
+      f.input :email, :label => "E-mail"
+      f.input :cuit, :label => "Cuit"
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row "Nombre" do
+        resource.name
+      end
+      row "E-mail" do
+        resource.email
+      end
+      row "Cuit" do
+        resource.cuit
+      end
+    end
+  end
 
   filter :name, :label => "Nombre"
   filter :cuit, :label => "Cuit"
