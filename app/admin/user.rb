8e5129900e9b395d814e0ccfb12513fbed30ba28
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2015-12-10 12:05:21 -0300

    Customized users table, create/update, filters and redirections.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,27 +1,39 @@
 ActiveAdmin.register User do
   permit_params :email, :password, :password_confirmation
-
+  controller do
+    def show
+      redirect_to edit_home_user_path(resource)
+    end
+    def update
+      update!{ home_users_path }
+    end
+    def create
+      create!{ home_users_path }
+    end
+  end
   index do
     selectable_column
-    id_column
-    column :email
-    column :type
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
-    actions
+    column "E-mail", :email
+    column "Tipo", :type
+    column "Última sesión", :current_sign_in_at
+    column "Creado el", :created_at
+    column "Acciones" do |exposition|
+      span do 
+        link_to 'Editar', edit_home_user_path(exposition), :method => :get
+      end
+      span do 
+        link_to 'Eliminar', home_user_path(exposition), :method => :delete, :data => { :confirm => '¿Estás seguro de eliminar este usuario?' }
+      end
+    end
   end
 
-  filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
-
+  filter :email, :label => "E-mail"
+  filter :type, :label => "Tipo de usuario"
   form do |f|
     f.inputs "Admin Details" do
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
+      f.input :email, :label => "E-mail"
+      f.input :password, :label => "Contraseña"
+      f.input :password_confirmation, :label => "Confirmar contraseña"
     end
     f.actions
   end
