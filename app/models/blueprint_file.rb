fd65a301c246581346f93f20df18ba330d5036e0
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2015-12-11 15:31:31 -0300

    Setted infrastructure & blueprint files

--- app/models/blueprint_file.rb
+++ app/models/blueprint_file.rb
@@ -1,3 +1,11 @@
 class BlueprintFile < ActiveRecord::Base
   belongs_to :infrastructure
+  has_attached_file :attachment
+  validates_attachment :attachment,
+  	content_type: {
+  		content_type: ['image/jpg', 'image/jpeg', 'image/png', 'application/pdf']
+	},
+	size: {
+  		in: 0..5.megabytes
+  	}
 end
--- app/models/infrastructure.rb
+++ app/models/infrastructure.rb
@@ -1,4 +1,5 @@
 class Infrastructure < ActiveRecord::Base
   belongs_to :expositor
   has_many :blueprint_files, :dependent => :destroy
+  accepts_nested_attributes_for :blueprint_files
 end
