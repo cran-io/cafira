58e06ed682355509172f7eb27a030a5bee0b7193
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2015-12-15 16:41:40 -0300

    Changing infrastructure & blueprint file status when approving/desapproving or uploading blueprint files

--- app/models/infrastructure.rb
+++ app/models/infrastructure.rb
@@ -8,9 +8,20 @@ class Infrastructure < ActiveRecord::Base
   def verify_fields
     status = true
     blueprint_files.each do |blueprint_file|
-      status = false if blueprint_file.attachment_file_name.nil?
-  	end
-    self.completed = status 
+      if blueprint_file.attachment_file_name.nil?
+        status = false 
+      elsif blueprint_file.attachment_file_name_changed?
+        if blueprint_file.state == false
+          blueprint_file.state = nil
+        end
+      else
+        if blueprint_file.state == false
+          status = false
+        end
+      end
+    end
+    status = true if blueprint_files.where(:state => nil).count == 2
+    self.completed = status
     nil
   end
 
