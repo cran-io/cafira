aef8eb6cbe48badefdb84d5b115f067afc76332a
Santiago Musielack <santiaggoo@gmail.com>
2015-12-18 16:11:38 -0300

    upper bound limit addded to near_deadline method

--- app/models/expositor.rb
+++ app/models/expositor.rb
@@ -9,10 +9,10 @@ class Expositor < User
   #returns a Set of all expositors with uncompleted tasks and without much time to complete them
   def self.near_deadline
     aSetofExpositors = Set.new
-    aSetofExpositors.merge(self.joins(:catalog).where("catalogs.completed = ?",false).joins(:expositions).where("expositions.deadline_catalogs <= ? AND expositions.active = ?",Date.today + 2.days,true))
-    aSetofExpositors.merge(self.joins(:credentials).where(:credentials => {:id => nil}).joins(:expositions).where("expositions.deadline_credentials <= ? AND expositions.active = ?",Date.today + 2.days,true))
-    aSetofExpositors.merge(self.joins(:aditional_service).where("aditional_services.completed =?",false).joins(:expositions).where("expositions.deadline_aditional_services <= ? AND expositions.active = ?",Date.today + 2.days,true))
-    aSetofExpositors.merge(self.joins(:infrastructure).where("infrastructures.completed =?",false).joins(:expositions).where("expositions.deadline_infrastructures <= ? AND expositions.active = ?",Date.today + 2.days,true))
+    aSetofExpositors.merge(self.joins(:catalog).where("catalogs.completed = ?",false).joins(:expositions).where("expositions.deadline_catalogs >= ? AND expositions.deadline_catalogs <= ? AND expositions.active = ?",Date.today,Date.today + 2.days,true))
+    aSetofExpositors.merge(self.joins(:credentials).where(:credentials => {:id => nil}).joins(:expositions).where("expositions.deadline_credentials >= ? AND expositions.deadline_credentials <= ? AND expositions.active = ?",Date.today,Date.today + 2.days,true))
+    aSetofExpositors.merge(self.joins(:aditional_service).where("aditional_services.completed =?",false).joins(:expositions).where("expositions.deadline_aditional_services >= ? AND expositions.deadline_aditional_services <= ? AND expositions.active = ?",Date.today,Date.today + 2.days,true))
+    aSetofExpositors.merge(self.joins(:infrastructure).where("infrastructures.completed =?",false).joins(:expositions).where("expositions.deadline_infrastructures >= ? AND expositions.deadline_infrastructures <= ? AND expositions.active = ?",Date.today,Date.today + 2.days,true))
     aSetofExpositors
   end
 
