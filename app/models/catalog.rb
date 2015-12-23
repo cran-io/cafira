194f913a49bf2baba3982c0275b9a432a659c81f
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2015-12-23 11:39:42 -0300

    Generating xslx and zipping it with catalog images

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -4,7 +4,37 @@ class Catalog < ActiveRecord::Base
   accepts_nested_attributes_for :catalog_images
   before_update :verify_fields
 
+  def download_catalog
+    filename = 'datos_catalogo.zip'
+    temp_file = Tempfile.new(filename)
+    catalog_data = generate_xslx
+    Zip::OutputStream.open(temp_file) { |zos| }
+    Zip::File.open(temp_file.path, Zip::File::CREATE) do |zip|
+      zip.add('datos_catalogo.xlsx', catalog_data.path)
+      catalog_images.each_with_index do |catalog_image, index|
+        zip.add(index.to_s + catalog_image.attachment_file_name, catalog_image.attachment.path)
+      end
+    end
+    temp_file
+  end
+
+
   private
+  
+  def generate_xslx
+    headers = ['Nro stand', 'Website', 'Twitter', 'Facebook', 'Tel 1', 'Tel 2', 'Email', 'Email adicional', 'Dirección', 'Ciudad', 'Provincia', 'Código postal']
+    catalog_data  = [ stand_number, website, twitter, facebook, phone_number, aditional_phone_number, email, aditional_email, address, city, province, zip_code]
+
+    package = Axlsx::Package.new
+    package.workbook.add_worksheet(:name => "Catálogo") do |sheet|
+      sheet.add_row headers        
+      sheet.add_row catalog_data 
+    end
+    tmpfile = Tempfile.new('catalogo')
+    package.serialize(tmpfile)
+    tmpfile
+  end
+  
   def verify_fields
     status = true
     catalog_images.each do |catalog_image|
