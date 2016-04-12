3f56869d564256e4a1db5b5a3ad48eb2cc794e01
Gabriel Silvatici <gsilvati@itba.edu.ar>
2016-04-12 17:41:05 -0300

    added toggle to inputs in aditional services

--- app/assets/javascripts/fields_management.js
+++ app/assets/javascripts/fields_management.js
@@ -1,13 +1,29 @@
 $(function() {
-  if(!$('#infrastructure_alfombra_input #infrastructure_alfombra').val()) {
+  if($('#infrastructure_alfombra_input #infrastructure_alfombra').val() != "true") {
     $('#infrastructure_alfombra_tipo_input').hide();
   }
+  if($('#aditional_service_energia_input #aditional_service_energia').val() != "true") {
+    $('#aditional_service_energia_cantidad_input').hide();
+  }
+  if($('#aditional_service_estacionamiento_input #aditional_service_estacionamiento').val() != "true") {
+    $('#aditional_service_estacionamiento_cantidad_input').hide();
+  }
+
   $('#infrastructure_alfombra_tipo').val() == 'otra' ? $('#infrastructure_alfombra_tipo_input .inline-hints').show() : $('#infrastructure_alfombra_tipo_input .inline-hints').hide();
   $('#infrastructure_alfombra_input #infrastructure_alfombra').click(function() {
     $('#infrastructure_alfombra_input #infrastructure_alfombra').val() == "true" ? $('#infrastructure_alfombra_tipo_input').show() : $('#infrastructure_alfombra_tipo_input').hide();
   });
   $('#infrastructure_alfombra_tipo_input #infrastructure_alfombra_tipo').change(function() {
     $('#infrastructure_alfombra_tipo').val() == 'otra' ? $('#infrastructure_alfombra_tipo_input .inline-hints').show() : $('#infrastructure_alfombra_tipo_input .inline-hints').hide();
+  });
+
+  $('#aditional_service_energia_input #aditional_service_energia').click(function() {
+    $('#aditional_service_energia_input #aditional_service_energia').val() == "true" ? $('#aditional_service_energia_cantidad_input').show() : $('#aditional_service_energia_cantidad_input').hide();
+  });
+
+
+  $('#aditional_service_estacionamiento_input #aditional_service_estacionamiento').click(function() {
+    $('#aditional_service_estacionamiento_input #aditional_service_estacionamiento').val() == "true" ? $('#aditional_service_estacionamiento_cantidad_input').show() : $('#aditional_service_estacionamiento_cantidad_input').hide();
   });
 
 });
