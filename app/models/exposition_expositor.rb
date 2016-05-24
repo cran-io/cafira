74800aa4cc006740333f1b96b3a14d825e138efc
jota <juanbanelli@gmail.com>
2016-05-24 17:47:55 -0300

    Cleaning expositors when assigning new exposition

--- app/models/exposition_expositor.rb
+++ app/models/exposition_expositor.rb
@@ -1,4 +1,15 @@
 class ExpositionExpositor < ActiveRecord::Base
   belongs_to :exposition
   belongs_to :expositor
+
+  before_create :init_expositor
+
+  private
+
+  def init_expositor
+    expositor = Expositor.find(self.expositor_id)
+    expositor.destroy_expositor_info
+    expositor.build_expositor
+  end
+
 end
