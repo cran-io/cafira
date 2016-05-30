b277ae1d995ca314dcfb0366b978a1b0d0b4e3b1
jota <juanbanelli@gmail.com>
2016-05-30 17:08:02 -0300

    Showing blueprintifle name in infrastructure form

--- app/admin/infrastructure.rb
+++ app/admin/infrastructure.rb
@@ -149,23 +149,23 @@ ActiveAdmin.register Infrastructure do
           status = '(NO APROBADO). Debe volver a subir el plano.'
           label  = 'label-red'
           ff.input :attachment, :label => "Plano <span class='#{label}'>#{status}</span>".html_safe, :as => :file, :require => false,
-          :hint => ff.object.attachment.present? ?  link_to_conversation.html_safe : content_tag(:span, "No hay un plano subido aún")
+          :hint => ff.object.attachment.present? ? (ff.object.attachment_file_name + " (" +  link_to_conversation + ")").html_safe : content_tag(:span, "No hay un plano subido aún")
         when 1
           status = '(APROBADO)'
           label  = 'label-green'
           label_tag 'name'
           ff.input :attachment_file_name, :label => "Plano <span class='#{label}'>#{status}</span>".html_safe, :input_html => { :disabled => true },
-          :hint => ff.object.comments_to_json == 'empty' ? 'No se ha iniciado una conversación aun' : link_to_conversation.html_safe
+          :hint => ff.object.attachment.present? ? (ff.object.attachment_file_name + " (" +  link_to_conversation + ")").html_safe : content_tag(:span, "No hay un plano subido aún")
         when 2
           status = '(PRE APROBADO)'
           label  = 'label-grey'
           ff.input :attachment, :label => "Plano <span class='#{label}'>#{status}</span>".html_safe, :as => :file, :require => false,
-          :hint => ff.object.attachment.present? ? link_to_conversation.html_safe : content_tag(:span, "No hay un plano subido aún")
+          :hint => ff.object.attachment.present? ? (ff.object.attachment_file_name + " (" +  link_to_conversation + ")").html_safe : content_tag(:span, "No hay un plano subido aún")
         else
           status = '(PENDIENTE A REVISIÓN)'
           label  = 'label-orange'
           ff.input :attachment, :label => "Plano <span class='#{label}'>#{status}</span>".html_safe, :as => :file, :require => false,
-          :hint => ff.object.attachment.present? ?  link_to_conversation.html_safe : content_tag(:span, "No hay un plano subido aún")
+          :hint => ff.object.attachment.present? ? (ff.object.attachment_file_name + " (" +  link_to_conversation + ")").html_safe : content_tag(:span, "No hay un plano subido aún")
         end
       end
     end
